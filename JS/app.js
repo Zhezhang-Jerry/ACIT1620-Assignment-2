0f59894aeec871778b33c58d8d98e84794f3c2cc
Jerry <zzhang155@my.bcit.ca>
2021-04-01 15:16:50 -0700

    Turn back the display from flex to grid in both html

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,14 @@
 /* function to make a real slidebar */
+function sliderbar () {
+
+}
 
 /* function to make a real textarea which can be saved */
+function textarea () {
+
+}
 
 /* function to make those button as a real functional button */
+function button () {
+
+}
