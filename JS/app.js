403726f6947c501a2c4a472b176d5692a5acb496
Jerry <zzhang155@my.bcit.ca>
2021-04-01 15:33:12 -0700

    add the description of all functions

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,21 @@
-/* function to make a real slidebar */
+/* function to make a real slidebar
+1. link the sliderbar to both htmls.
+2. make the sliderbar work.
 function sliderbar () {
 
 }
 
-/* function to make a real textarea which can be saved */
+function to make a real textarea
+1. make the text can be saved.
+2. make the text can be clean.
 function textarea () {
 
 }
 
-/* function to make those button as a real functional button */
+function to make those button as a real functional button
+1. link all button to corresponding page.
+2. make all buttons work.
 function button () {
 
 }
+ */
