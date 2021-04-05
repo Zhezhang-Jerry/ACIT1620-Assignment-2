247e5f42a3005692f9a9746461ab6a124e15d27f
Jerry <zzhang155@my.bcit.ca>
2021-04-05 11:17:24 -0700

    Add slideout and slidein function in app.js for slideout menu

--- JS/app.js
+++ JS/app.js
@@ -1,11 +1,20 @@
-/* function to make a real slidebar
-1. link the sliderbar to both htmls.
-2. make the sliderbar work.
-function sliderbar () {
-
+function slideout() {
+    document.getElementById("slidemenu").style.display = 'block';
+    document.getElementById("buttonright").style.display = 'none';
+    document.getElementById("buttonleft").style.display = 'block';
+    document.getElementById("sidebarmenu").style.backgroundColor = '#faf4ed';
+}
+function slidein() {
+    document.getElementById("slidemenu").style.display = 'none';
+    document.getElementById("buttonright").style.display = 'block';
+    document.getElementById("buttonleft").style.display = 'none';
+    document.getElementById("sidebarmenu").style.backgroundColor = '#f2e9de';
 }
 
-function to make a real textarea
+
+
+
+/* function to make a real textarea
 1. make the text can be saved.
 2. make the text can be clean.
 function textarea () {
