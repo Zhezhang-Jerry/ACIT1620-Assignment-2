f159653517cebbaa7dd09cfa4f19b834961b536f
JerryZhang <zhangzhe941020@yahoo.com>
2021-04-10 15:22:59 -0700

    use queryselector and insertadjacentHtml to help functions show the textarea

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,82 @@
-/* function to make a real slidebar */
-function sliderbar () {
+const notesArray = []
 
+function AddnotesSec() {
+    html = `
+    <div id="note-area">
+        <textarea id="addnotes" >Your note here.</textarea>
+        <div class="itemc2" id="itemcc2">
+        <button class="sd" onclick="saveNote()">save</button>
+        <button class="sd" onclick="deleteNote()">delete</button></div>
+    </div>
+    `
+    return html
 }
 
-/* function to make a real textarea which can be saved */
-function textarea () {
-
+function createNoteWArea() {
+    const div = document.querySelector('#content')
+    div.insertAdjacentHTML('beforeend', AddnotesSec())
+    setMouse()
+    document.getElementById("createnotes").style.display = 'none';
 }
 
-/* function to make those button as a real functional button */
-function button () {
-
+function setMouse() {
+    const div = document.querySelector('#addnotes')
+    div.focus()
 }
+
+function saveNote() {
+    const div = document.querySelector('#addnotes')
+    const title = div.firstChild.textContent
+    const body = convertTexttoString()
+    notesArray.push(addNote(title, body))
+    deleteNote()
+}
+
+function addNote(title, body) {
+    return { title, body }
+}
+
+function convertTexttoString() {
+    let str = ""
+    const divs = [...document.querySelectorAll('[contenteditable] > div:not(:first-child)')]
+    for (const i of divs) {
+        str += `${i.textContent}\n`
+    }
+    return str
+}
+
+function deleteNote() {
+    const div = document.querySelector('#note-area')
+    div.remove()
+}
+
+
+
+// function slideout() {
+//     document.getElementById("slidemenu").style.display = 'block';
+//     document.getElementById("buttonleft").style.backgroundColor = '#191724';
+//     document.getElementById("buttonright").style.display = 'none';
+//     document.getElementById("buttonleft").style.display = 'block';
+//     document.getElementById("sidebarmenu").style.backgroundColor = '#faf4ed';
+// }
+// function slidein() {
+//     document.getElementById("slidemenu").style.display = 'none';
+//     document.getElementById("buttonright").style.display = 'block';
+//     document.getElementById("buttonleft").style.display = 'none';
+//     document.getElementById("sidebarmenu").style.backgroundColor = '#f2e9de';
+// }
+// function popout() {
+//     document.getElementById("addnotes").style.display = 'block';
+//     document.getElementById("itemcc2").style.display = 'block';
+//     document.getElementById("createnotes").style.display = 'none';
+//     document.getElementById("plus1").style.display = "none";
+//     document.getElementById("plus2").style.display = "block";
+//     document.getElementById("plus2").style.backgroundColor = "#191724";
+// }
+// function popin() {
+//     document.getElementById("addnotes").style.display = 'none';
+//     document.getElementById("itemcc2").style.display = 'none';
+//     document.getElementById("createnotes").style.display = 'block';
+//     document.getElementById("plus1").style.display = "block";
+//     document.getElementById("plus2").style.display = "none";
+// }
