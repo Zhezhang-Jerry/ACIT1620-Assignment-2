3aa948c3bfa70dd1c2b4a3022a9d1c8e3ee3d53a
JerryZhang <zhangzhe941020@yahoo.com>
2021-04-10 22:39:28 -0700

    delete comment code

--- JS/app.js
+++ JS/app.js
@@ -34,9 +34,6 @@ function saveNote() {
     createNoteObject(notearr)
     let output_textarea = document.querySelector('#note1')
     output_textarea.textContent = notearr[0].value;
-    // const title = note.firstChild.textContent
-    // const body = convertTexttoString()
-    // notesArray.push(addNote(title, body))
     document.getElementById("createnotes").style.display = 'block';
     slideout()    
     deleteNote()
@@ -49,15 +46,6 @@ function createNoteObject(arr) {
 function addNote(title, body) {
     return { title, body }
 }
-
-// function convertTexttoString() {
-//     let str = ""
-//     const divs = [...document.querySelectorAll('[contenteditable] > div:not(:first-child)')]
-//     for (const i of divs) {
-//         str += `${i.textContent}\n`
-//     }
-//     return str
-// }
 
 function deleteNote() {
     const div = document.querySelector('#note-area')
@@ -109,46 +97,3 @@ darkmode.addEventListener('click', function () {
         countclick = 0;}
     }
     }});
-
-
-
-
-
-
-
-// function turnDark() {
-//     if (countclick == 0) {
-//     document.getElementById("header").style.backgroundColor = '#1f1d2e';
-//     document.getElementById("footer").style.backgroundColor = '#1f1d2e';
-//     document.getElementById("sidebarmenu").style.backgroundColor = '#1f1d2e';
-//     document.getElementById("icontent").style.backgroundColor = '#1f1d2e';
-//     document.getElementById("buttonleft").style.backgroundColor = '#1f1d2e';
-//     document.getElementById("buttonright").style.backgroundColor = '#1f1d2e';
-//     document.getElementById('bdy').style.color = '#fffaf3';
-//     countclick = 1;}
-//     else {
-//         document.getElementById("header").style.backgroundColor = '#f2e9de';
-//     document.getElementById("footer").style.backgroundColor = '#f2e9de';
-//     document.getElementById("sidebarmenu").style.backgroundColor = '#f2e9de';
-//     document.getElementById("icontent").style.backgroundColor = '#f2e9de';
-//     document.getElementById("buttonleft").style.backgroundColor = '#f2e9de';
-//     document.getElementById("buttonright").style.backgroundColor = '#f2e9de';
-//     document.getElementById('bdy').style.color = '#1f1d2e';
-//     countclick = 0;
-//     }}
-
-// function popout() {
-//     document.getElementById("addnotes").style.display = 'block';
-//     document.getElementById("itemcc2").style.display = 'block';
-//     document.getElementById("createnotes").style.display = 'none';
-//     document.getElementById("plus1").style.display = "none";
-//     document.getElementById("plus2").style.display = "block";
-//     document.getElementById("plus2").style.backgroundColor = "#191724";
-// }
-// function popin() {
-//     document.getElementById("addnotes").style.display = 'none';
-//     document.getElementById("itemcc2").style.display = 'none';
-//     document.getElementById("createnotes").style.display = 'block';
-//     document.getElementById("plus1").style.display = "block";
-//     document.getElementById("plus2").style.display = "none";
-// }
