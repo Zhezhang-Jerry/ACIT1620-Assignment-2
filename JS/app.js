55b33592774c0466e53d8bbccbb5dda0453d6abc
JerryZhang <zhangzhe941020@yahoo.com>
2021-04-10 22:17:23 -0700

    fix merge bugs

--- JS/app.js
+++ JS/app.js
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-function slideout() {
-    document.getElementById("slidemenu").style.display = 'block';
-    document.getElementById("buttonleft").style.backgroundColor = '#191724';
-    document.getElementById("buttonright").style.display = 'none';
-    document.getElementById("buttonleft").style.display = 'block';
-    document.getElementById("sidebarmenu").style.backgroundColor = '#faf4ed';
-}
-function slidein() {
-    document.getElementById("slidemenu").style.display = 'none';
-    document.getElementById("buttonright").style.display = 'block';
-    document.getElementById("buttonleft").style.display = 'none';
-    document.getElementById("sidebarmenu").style.backgroundColor = '#f2e9de';
-}
-function popout() {
-    document.getElementById("addnotes").style.display = 'block';
-    document.getElementById("itemcc2").style.display = 'block';
-    document.getElementById("createnotes").style.display = 'none';
-    document.getElementById("plus1").style.display = "none";
-    document.getElementById("plus2").style.display = "block";
-    document.getElementById("plus2").style.backgroundColor = "#191724";
-}
-function popin() {
-    document.getElementById("addnotes").style.display = 'none';
-    document.getElementById("itemcc2").style.display = 'none';
-    document.getElementById("createnotes").style.display = 'block';
-    document.getElementById("plus1").style.display = "block";
-    document.getElementById("plus2").style.display = "none";
-}
-
-
-
-/* function to make a real textarea
-1. make the text can be saved.
-2. make the text can be clean.
-function textarea () {
-=======
 const notesArray = []
 
 function AddnotesSec() {
@@ -109,7 +72,6 @@ function slideout() {
     document.getElementById("buttonleft").style.display = 'block';
     document.getElementById("sidebarmenu").style.backgroundColor = '#faf4ed';
 }
->>>>>>> a2
 
 function slidein() {
     document.getElementById("slidemenu").style.display = 'none';
@@ -118,15 +80,6 @@ function slidein() {
     document.getElementById("sidebarmenu").style.backgroundColor = '#f2e9de';
 }
 
-<<<<<<< HEAD
-function to make those button as a real functional button
-1. link all button to corresponding page.
-2. make all buttons work.
-function button () {
-
-}
- */
-=======
 let header = document.getElementById('header');
 let footer =  document.getElementById("footer");
 let sidebarmenu = document.getElementById("sidebarmenu");
@@ -199,4 +152,3 @@ darkmode.addEventListener('click', function () {
 //     document.getElementById("plus1").style.display = "block";
 //     document.getElementById("plus2").style.display = "none";
 // }
->>>>>>> a2
